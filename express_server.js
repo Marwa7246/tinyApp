a3cd8110410ca66787a991a3874a9bdb335f4413
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-08-03 22:16:02 +0000

    Install and setup EJS

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
+ 
 const express = require('express');
 const app = express();
 const PORT = 8080;
+app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,8 +19,8 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.send('<html><body> Hello <b>World</b></body></html>\n')
-})
+  res.send('<html><body> Hello <b>World</b></body></html>\n');
+});
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
-})
+  console.log(`Example app listening on port ${PORT}!`);
+});
